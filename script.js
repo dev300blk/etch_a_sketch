71ed933a18a9ece99dfb2ce46c07a52f2da08541
Brandon Romaine <brandonmromaine@gmail.com.com>
2022-04-13 14:56:15 -0600

    20X20 grid dynamically created. Reset button

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert('Etch AAAAAA Sketch');
+const board = document.querySelector('.container');
+const resetbtn = document.querySelector('.resetbtn');
+console.log(resetbtn)
+
+// reset event
+resetbtn.addEventListener('click',()=>{
+console.log('click')
+reset();
+})
+
+// fills board with empty cells
+function reset(){
+function createElement(){
+   const cell =  document.createElement('div');
+   cell.classList.add('cell');
+   board.appendChild(cell);
+}
+
+for(i=0;i<400;i++){
+   // console.log(i);
+   createElement();
+}
+}
+
