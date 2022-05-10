1a8fe2e573dc78cc8db8e8ec51bc41fde17f57ce
Brandon Romaine <brandonmromaine@gmail.com.com>
2022-05-09 20:53:40 -0600

    Added css variables to set grid rows and column.

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 const board = document.querySelector('.container');
 const resetbtn = document.querySelector('.resetbtn');
 const root = document.querySelector(':root');
-console.log(root);
+
 
 let pro = prompt('Choose a multiple of ten between 10, and 100');
 
 
 
-function prom() {
+
+       
+       function changeGrid(){ 
         let colNum = window.getComputedStyle(document.documentElement).getPropertyValue('--colNum');
         let rowNum = window.getComputedStyle(document.documentElement).getPropertyValue('--rowNum');
         let cellH = window.getComputedStyle(document.documentElement).getPropertyValue('--cellH');
         let cellW = window.getComputedStyle(document.documentElement).getPropertyValue('--cellW');
-        root.style.setProperty('--colNum', pro);
-        root.style.setProperty('--rowNum', pro);
-        // root.style.setProperty('--cellH', pro);
-        // root.style.setProperty('--cellW', pro);
-        console.log(colNum, rowNum, cellH, cellW);
-}
+        document.documentElement.style.setProperty('--colNum', pro);
+        document.documentElement.style.setProperty('--rowNum', pro);
+       }       
 
-prom();
-
+changeGrid();
+ 
 let iterable = pro * pro;
 
 console.log(iterable);
